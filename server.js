ef04d4c15b88bf3a62b0ab90a3ae95ce4958cf74
Zijian Ding <zding@eng.ucsd.edu>
2019-02-18 14:37:24 -0800

    change food & add item

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 var fs = require('fs');
 
-var dataLoad = fs.readFileSync('website/js/data.json');
+//var item = require('website/js/data.json');
 
-//change it to json
-var itemHave = JSON.parse(dataLoad);
+function readJSONdata(jsonfilename){
+    var dataLoad = fs.readFileSync(jsonfilename);
+    //change it to json
+    var itemHave = JSON.parse(dataLoad);
+    return itemHave;
+}
 
-console.log(itemHave);
+itemHave = readJSONdata('website/js/data.json');
 
 var express = require('express');
 
@@ -19,23 +23,29 @@ function listening() {
 
 app.use(express.static('website'));
 
-app.get('/add/:name/:price', addNewItem);
+app.get('/add/:name/:price/:expireDate/:weight/:unit/:type', addNewItem);
 
 function addNewItem(request, response){
     var data = request.params;
     var name = data.name;
     var price = Number(data.price);
-    var reply;
-    
-    if(!price){
-        reply = {msg: "The price is required."};
+    var expireDate = Number(data.expireDate);
+    var weight = Number(data.weight);
+    var unit = data.unit;
+    var type = data.type;
+    var reply; 
+    if(!name){
+        reply = {msg: "The name is required."};
         response.send(reply)
     } else 
     {
-    
     var newItem = {
         "name": name,
-        "price": price
+        "price": price,
+        "expireDate": expireDate,
+        "weight": weight,
+        "unit": unit,
+        "type": type,
     }
         fs.readFile('website/js/data.json',function(err, data){
             if(err){
@@ -53,6 +63,28 @@ function addNewItem(request, response){
             response.send(reply)
     })
     }
+}
+
+app.get('/del/:delID', delItem);
+
+function delItem(request, response){
+    var data = request.params;
+    var delID= data.delID;
+    fs.readFile('website/js/data.json',function(err, data){
+        if(err){
+            return console.error(err);
+        }
+        var allItem = data.toString();
+        allItem = JSON.parse(allItem);
+        allItem.item.splice(delID,1);
+        var newData = JSON.stringify(allItem, null, 2);
+        fs.writeFile('website/js/data.json', newData, finished);
+        function finished(err){
+            console.log('Done');
+        };
+        reply = {msg: "Your item is used."};
+        response.send(reply);
+    })
 }
 
 app.get('/all', sendAll);
--- website/js/additem.js
+++ website/js/additem.js
@@ -4,17 +4,59 @@ $(document).ready(function() {
 
 function setup(){
 	console.log('Running');
-	$('#newSubmit').click(putNewItem);
+	$('.newSubmit').click(putNewItem);
 }
 
 function putNewItem(){
 	var name = document.getElementById('name').value;
 	var price = Number(document.getElementById('price').value);
+	var expireDate = Date(document.getElementById('expireDate').value);
+	var weight = Number(document.getElementById('weight').value);
+	var unit = document.getElementById('unit').value;
+	var type = document.getElementById('type').value;
 	console.log('Adding');
-	$.get('add/'+name+'/'+price, finished);
+	$.get('add/'+name+'/'+price+'/'+expireDate+'/'+weight+'/'+unit+'/'+type, finishedAdd);
+	$(this).text("Gotcha! Add one more");
+	document.getElementById('name').value = ' ';
+	document.getElementById('price').value = ' ';
+	document.getElementById('expireDate').value = ' ';
+	document.getElementById('weight').value = ' ';
+}
+
+function itemDetail(){
+	var selID = $(this)[0].value;
+	console.log(document.getElementById("recentItem"+selID).className);
+	if (document.getElementById("recentItem"+selID).className == 0){
+		var xhttpitem = new XMLHttpRequest();
+			xhttpitem.onreadystatechange = function() {	
+				if (this.readyState == 4 && this.status == 200) {
+					console.log('Loading json to get item info from the server');
+					var response = JSON.parse(xhttpitem.responseText);
+					var item = response.item;
+					document.getElementById("recentItem"+selID).append("Price: $"+item[selID].price
+					+" Size: "+item[selID].weight+" "+item[selID].unit);
+				}
+			};
+		xhttpitem.open("GET", "/js/data.json", true);
+		xhttpitem.send();
+		document.getElementById("recentItem"+selID).className = 1;
+	} else {
+		$("#recentItem"+selID).fadeToggle();
+	}
+}
+
+function delItem(){
+	var delID = $(this)[0].value;
+	console.log(delID);
+	$.get('del/'+delID, finishedDel);
+}
+
+function finishedAdd(data) {
+	console.log("Your new item is recorded");
 	
 }
 
-function finished(data) {
-	location.reload(true);
+function finishedDel(data) {
+	console.log("Your new item is deleted");
+	setTimeout(function(){ window.location.reload(true); }, 1000);
 }
