e0431f38374a5575a458a5b3118d5ac476120695
Zijian Ding <zding@eng.ucsd.edu>
2019-03-07 21:37:24 -0800

    facebook login

--- website/js/facebook.js
+++ website/js/facebook.js
@@ -14,7 +14,7 @@ function statusChangeCallback(response) {
   if (response.status === 'connected') {
     // Logged into your app and Facebook.
     $(".facebookLogin").hide();
-    window.open("home.html","_self")
+    window.open("home_test.html","_self")
     console.log('Successfully logged in with Facebook');
     FB.api('/me?fields=name,first_name,picture.width(480)', changeUser);
   }
